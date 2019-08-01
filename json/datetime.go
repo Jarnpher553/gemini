a0e13285f0f8f273b32c52c41b8abc2c8851ea29
jarnpher__lee <jarnpher_553@foxmail.com>
2019-08-01 22:41:03 +0800

    修改

--- json/datetime.go
+++ json/datetime.go
@@ -4,24 +4,26 @@ import (
 	"time"
 )
 
-type Date time.Time
+type Date struct {
+	time.Time
+}
 
 func (date *Date) MarshalJSON() ([]byte, error) {
-	if *date == Date(time.Time{}) {
+	if (*date).Time == (time.Time{}) {
 		return []byte(`""`), nil
 	}
 
-	t := time.Time(*date)
+	t := (*date).Time
 	return []byte(`"` + t.Format("2006-01-02") + `"`), nil
 }
 
 func (date *Date) String() string {
-	return time.Time(*date).Format("2006-01-02")
+	return (*date).Time.Format("2006-01-02")
 }
 
 func (date *Date) UnmarshalJSON(data []byte) (err error) {
 	d := string(data[1 : len(data)-1])
 	t, err := time.ParseInLocation("2006-01-02", string(d), time.Local)
-	*date = Date(t)
+	*date = Date{Time: t}
 	return err
 }
