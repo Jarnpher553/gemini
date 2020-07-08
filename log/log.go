b41bbd8bdc4f0611f20499e8cedf820b1c447c85
李剑峰 <lijianfeng@shinsson.com>
2020-07-08 12:56:29 +0800

    refactor:修改日志格式为json

--- log/log.go
+++ log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 	"io"
 	"runtime"
@@ -33,12 +32,14 @@ func init() {
 		Logger: logrus.New(),
 	}
 
-	Logger.SetFormatter(&nested.Formatter{
-		HideKeys:        true,
-		TimestampFormat: "2006-01-02 15:04:05",
-		ShowFullLevel:   true,
-		TrimMessages:    true,
-	})
+	//Logger.SetFormatter(&nested.Formatter{
+	//	HideKeys:        true,
+	//	TimestampFormat: "2006-01-02 15:04:05",
+	//	ShowFullLevel:   true,
+	//	TrimMessages:    true,
+	//})
+
+	Logger.SetFormatter(&logrus.JSONFormatter{})
 
 	Logger.SetReportCaller(true)
 
