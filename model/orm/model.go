dd96237e64e3efd0ad170c964ec30cb98d6a1d1c
李剑峰 <lijianfeng@shinsson.com>
2020-08-07 12:39:25 +0800

    fix:orm时间默认值

--- model/orm/model.go
+++ model/orm/model.go
@@ -8,17 +8,17 @@ import (
 
 // ModelInt int主键
 type ModelInt struct {
-	ID          int `gorm:"primary_key;AUTO_INCREMENT"`
-	CreatedTime time.Time
-	UpdatedTime time.Time
-	IsActive    bool `gorm:"not null"`
+	ID          int       `gorm:"primary_key;AUTO_INCREMENT"`
+	CreatedTime time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedTime time.Time `gorm:"not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	IsActive    bool      `gorm:"not null"`
 }
 
 // ModelUUID uuid主键
 type ModelUUID struct {
 	ID          uuid.GUID `gorm:"primary_key"`
-	CreatedTime time.Time `gorm:"not null"`
-	UpdatedTime time.Time `gorm:"not null"`
+	CreatedTime time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedTime time.Time `gorm:"not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 	IsActive    bool      `gorm:"not null"`
 }
 
