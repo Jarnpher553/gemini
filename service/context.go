59f6a1ebe0dacfa0337782d485a91c6f330c5e54
jarnpher_rice <jarnpher_553@foxmail.com>
2020-09-04 15:49:04 +0800

    refactor:修改请求体和成功日志打印

--- service/context.go
+++ service/context.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"github.com/Jarnpher553/gemini/erro"
 	"github.com/Jarnpher553/gemini/log"
 	"github.com/Jarnpher553/gemini/model/dto"
@@ -10,7 +9,6 @@ import (
 	"github.com/Jarnpher553/gemini/uuid"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
 )
 
 type Ctx struct {
@@ -42,17 +40,10 @@ func (c *Ctx) FileStream(data []byte, filename string) {
 }
 
 func (c *Ctx) Success(data interface{}) {
-	dataStr := strings.TrimLeft(fmt.Sprintf("%+v", data), "&")
-	if len(dataStr) > 255 {
-		dataStr = dataStr[:255] + "..."
-	}
-	log.Zap.Caller(2).Info(log.Message(erro.ErrSuccess, erro.ErrMsg[erro.ErrSuccess], dataStr))
 	c.response(erro.ErrSuccess, data)
 }
 
 func (c *Ctx) Failure(code int, err error, actual ...bool) {
-	log.Zap.Caller(2).Error(log.Message(code, erro.ErrMsg[code], err))
-
 	if len(actual) != 0 && actual[0] {
 		c.response(code, nil, err.Error())
 	} else {
@@ -61,8 +52,6 @@ func (c *Ctx) Failure(code int, err error, actual ...bool) {
 }
 
 func (c *Ctx) Response(code int, data interface{}) {
-	log.Zap.Caller(2).Error(log.Message(code, erro.ErrMsg[code]))
-
 	c.response(code, data)
 }
 
@@ -71,6 +60,20 @@ func (c *Ctx) response(code int, data interface{}, err ...string) {
 	if len(err) != 0 && err[0] != "" {
 		e = err[0]
 	}
+
+	response := &dto.Response{
+		ErrCode:   code,
+		ErrMsg:    e,
+		Timestamp: now.New().Unix(),
+		Data:      data,
+	}
+
+	if code == 200 {
+		log.Zap.Caller(2).Info(log.Message(response.ErrCode, response.ErrMsg, response.Timestamp))
+	} else {
+		log.Zap.Caller(2).Error(log.Message(response.ErrCode, response.ErrMsg, response.Timestamp))
+	}
+
 	c.JSON(http.StatusOK, &dto.Response{
 		ErrCode:   code,
 		ErrMsg:    e,
@@ -121,17 +124,4 @@ func (c *Ctx) SetUser(user interface{}) {
 	var cc context.Context
 	cc = context.WithValue(c.Request.Context(), "auth_user", user)
 	c.Request = c.Request.WithContext(cc)
-}
-
-func (c *Ctx) Bind(obj interface{}) error {
-	err := c.ShouldBind(obj)
-
-	dataStr := strings.TrimLeft(fmt.Sprintf("%+v", obj), "&")
-	if len(dataStr) > 255 {
-		dataStr = dataStr[:255] + "..."
-	}
-
-	log.Zap.Caller(2).Info(dataStr)
-
-	return err
 }
